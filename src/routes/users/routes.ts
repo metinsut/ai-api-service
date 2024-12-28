e6afe23d4cb45fceee9ad138a84109a908072b61
Metin Süt <metindir@gmail.com>
2024-12-28 04:36:22 +0300

    update user docs

--- src/routes/users/routes.ts
+++ src/routes/users/routes.ts
@@ -1,19 +1,42 @@
-import { userInsertSchema } from "@/lib/db/schema";
+import { userInsertSchema, userSelectSchema } from "@/lib/db/schema";
 import { auth } from "@/middlewares/auth";
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { validator } from "hono-openapi/zod";
+import { resolver, validator } from "hono-openapi/zod";
 import { createUser, deleteUser, getAllUsers, getUser, updateUser } from "./controller";
+import { describeRoute } from "hono-openapi";
 
 const users = new OpenAPIHono();
 
 // Public routes
-users.post("/", validator("json", userInsertSchema), createUser);
+users.post(
+  "/",
+  describeRoute({
+    tags: ["Users"],
+    responses: {
+      201: {
+        description: "User created",
+        content: {
+          "application/json": {
+            schema: resolver(userSelectSchema),
+          },
+        },
+      },
+    },
+  }),
+  validator("json", userInsertSchema),
+  createUser,
+);
 
 // Protected routes
 users.use("/*", auth);
-users.get("/", getAllUsers);
-users.get("/:id", getUser);
-users.patch("/:id", validator("json", userInsertSchema), updateUser);
-users.delete("/:id", deleteUser);
+users.get("/", describeRoute({ tags: ["Users"] }), getAllUsers);
+users.get("/:id", describeRoute({ tags: ["Users"] }), getUser);
+users.patch(
+  "/:id",
+  describeRoute({ tags: ["Users"] }),
+  validator("json", userInsertSchema),
+  updateUser,
+);
+users.delete("/:id", describeRoute({ tags: ["Users"] }), deleteUser);
 
 export { users };
